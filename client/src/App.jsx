172f37d1f3d087f9de53fc73ad16233878b57db1
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-12-20 08:57:08 +0300

    folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Login from './components/Login'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Login/>
+    <h1 className="text-3xl text-center font-bold underline">
+      Hello world!
+    </h1>
+      
+    </>
+  )
+}
+
+export default App
