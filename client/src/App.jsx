b6ad4fda89f10289b8cc1c057cd540fc8f39bf40
wilcox <wilcox.ronguno@abri.une.edu.au>
2024-01-25 15:54:15 +1100

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,37 @@
 // import { useState } from 'react'
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/HomePage";
+import JockeyPage from "./pages/Jockey";
 import { Route, Routes } from "react-router-dom";
 // import MockLogin from "./components/Mockup";
-
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
 function App() {
-  
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
+  let nav;
+  if (isLoggedIn) {
+    nav = <Navbar />;
+  }
   return (
-    <div className="bg-[#F6FCFC] max-w-[1600px]  mx-auto rounded-sm ">
-      <Navbar />
+    <div className=" max-w-[1600px]  mx-auto rounded-sm ">
+      {nav}
       {/* <MockLogin/> */}
 
       <Routes>
-        
-      <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/jockey/:id" element={<JockeyPage />} />
       </Routes>
     </div>
   );
