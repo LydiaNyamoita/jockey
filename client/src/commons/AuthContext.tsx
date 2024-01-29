2edff334e586b8c161163bfafbdd2489dacd0ccb
wilcox <rongunowilcox@gmail.com>
2024-01-30 10:08:31 +1100

    add auth context and guard

---
+++ client/src/commons/AuthContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [jwt, setJwt] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const storedJwt = localStorage.getItem("jwt");
+    if (storedJwt) {
+      setIsLoggedIn(true);
+      setJwt(storedJwt);
+      // console.log('Set token'+)
+    }
+    setIsLoading(false);
+  }, []);
+  function login(token) {
+    setIsLoggedIn(true);
+    setJwt(token);
+    localStorage.setItem("jwt", token);
+  }
+
+  function logout() {
+    setIsLoggedIn(false);
+    setJwt(null);
+    localStorage.removeItem("jwt");
+  }
+
+  const authValue = {
+    isLoggedIn,
+    isLoading,
+    login,
+    logout,
+    jwt,
+  };
+
+  return (
+    <AuthContext.Provider value={authValue}>{children}</AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProvider };
---
+++ client/src/commons/RouteGuard.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import React from "react";
+
+function RouteGuard({ children }) {
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
+
+  if (isLoading) {
+    return <></>;
+  }
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default RouteGuard;
