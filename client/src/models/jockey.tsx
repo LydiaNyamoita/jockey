ebe8ff25d0e1f794d6b929066e6183e9b2819073
wilcox <rongunowilcox@gmail.com>
2024-01-30 10:10:57 +1100

    data models

---
+++ client/src/models/jockey.tsx
@@ -0,0 +1,10 @@
+export class Jockey {
+  created: string;
+  id: number;
+  name: string;
+  constructor(created: string, id: number, name: string) {
+    this.created = created;
+    this.id = id;
+    this.name = name;
+  }
+}
