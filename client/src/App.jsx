5c09e77f27cf1209a53417268ca0bbe000f0885f
wilcox <rongunowilcox@gmail.com>
2024-01-30 10:11:17 +1100

    use auth provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,67 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import JockeyPage from "./pages/Jockey";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 // import MockLogin from "./components/Mockup";
 import "bootstrap/dist/css/bootstrap.css";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "./commons/AuthContext";
+import RouteGuard from "./commons/RouteGuard";
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setLoggedIn(true);
-    } else {
-      setLoggedIn(false);
-    }
-  }, []);
+  const { isLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
   let nav;
+  let back_btn;
   if (isLoggedIn) {
     nav = <Navbar />;
+  }
+  const handleBack = () => {
+    navigate(-1);
+  };
+  if (location.pathname !== "/login" && location.pathname !== "/") {
+    back_btn = (
+      <button
+        className="bg-red-300 hover:bg-red-400 text-blac-800 font-bold py-2 px-2 rounded inline-flex items-center"
+        onClick={handleBack}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+        </svg>
+        <div className="pl-4"></div>
+        <span>Back</span>
+      </button>
+    );
   }
   return (
     <div className=" max-w-[1600px]  mx-auto rounded-sm ">
       {nav}
-      {/* <MockLogin/> */}
-
+      {back_btn}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <RouteGuard>
+              <HomePage />
+            </RouteGuard>
+          }
+        />
         <Route path="/login" element={<Login />} />
 
-        <Route path="/jockey/:id" element={<JockeyPage />} />
+        <Route
+          path="/jockey/:id"
+          element={
+            <RouteGuard>
+              <JockeyPage />
+            </RouteGuard>
+          }
+        />
       </Routes>
     </div>
   );
